b80ba69ab6ea8addd47f570cf41bbe324e13c456
Ramkumar Velmurugan <ram@Ramkumars-MacBook-Pro.local>
2025-02-22 12:49:16 -0500

    Add item field added

--- app/src/main/java/com/spkd/mvvm/database/MainActivity.kt
+++ app/src/main/java/com/spkd/mvvm/database/MainActivity.kt
@@ -1,32 +1,59 @@
 package com.spkd.mvvm.database
 
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.LinearLayout
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.spkd.mvvm.database.data.entitiy.Item
 import com.spkd.mvvm.database.ui.adapter.ItemAdapter
 import com.spkd.mvvm.database.ui.viewmodel.ItemViewModel
 
 @dagger.hilt.android.AndroidEntryPoint
-class MainActivity : androidx.appcompat.app.AppCompatActivity() {
-    private lateinit var viewModel: ItemViewModel
+class MainActivity : AppCompatActivity() {
+    private val viewModel: ItemViewModel by viewModels()
     private lateinit var adapter: ItemAdapter
 
-    override fun onCreate(savedInstanceState: android.os.Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(android.widget.LinearLayout(this).apply {
-            orientation = android.widget.LinearLayout.VERTICAL
-            val recyclerView = androidx.recyclerview.widget.RecyclerView(context).apply {
-                layoutManager = androidx.recyclerview.widget.LinearLayoutManager(context)
+
+        val layout = LinearLayout(this).apply {
+            orientation = LinearLayout.VERTICAL
+        }
+
+        val editText = EditText(this).apply {
+            hint = "Enter item name"
+        }
+
+        val button = Button(this).apply {
+            text = "Add Item"
+            setOnClickListener {
+                val itemName = editText.text.toString()
+                if (itemName.isNotBlank()) {
+                    viewModel.insert(Item(name = itemName))
+                    editText.text.clear()
+                }
             }
-            val button = android.widget.Button(context).apply {
-                text = "Add Item"
-                setOnClickListener { viewModel.insert(Item(name = "Item " + (1..100).random())) }
-            }
+        }
+
+        val recyclerView = RecyclerView(this).apply {
+            layoutManager = LinearLayoutManager(this@MainActivity)
+        }
+
+        adapter = ItemAdapter { viewModel.delete(it) }
+        recyclerView.adapter = adapter
+
+        layout.apply {
+            addView(editText)
             addView(button)
             addView(recyclerView)
-            adapter = ItemAdapter { viewModel.delete(it) }
-            recyclerView.adapter = adapter
-        })
+        }
 
-        viewModel = androidx.lifecycle.ViewModelProvider(this)[ItemViewModel::class.java]
+        setContentView(layout)
+
         viewModel.allItems.observe(this) { adapter.submitList(it) }
     }
 }
